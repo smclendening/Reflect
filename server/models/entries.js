86b4d206a4a1ec6ec3e7146f05a3079b842da8b2
Scott <smclendening@gmail.com>
2017-04-15 12:06:50 -0700

    Add server route and DB query to retrieve entries

--- server/models/entries.js
+++ server/models/entries.js
@@ -1,1 +1,8 @@
 const { db } = require('../../db/config.js');
+
+module.exports.findByUserId = (userId) => {
+  return db.manyOrNone('SELECT text, entries.entry_id, entries.created FROM entry_text INNER JOIN entries\
+  ON entry_text.entry_id = entries.entry_id\
+  WHERE entries.user_id = $1', [userId])
+}
+
