316ab518ba0a183c2eae8d0d08ca54e53b92768f
Scott <smclendening@gmail.com>
2017-04-15 15:37:46 -0700

    Add new method

--- server/models/entries.js
+++ server/models/entries.js
@@ -1,5 +1,14 @@
 const { db } = require('../../db/config.js');
 
+module.exports.new = (userId, audioId) => {
+  return db.one(
+    'INSERT INTO entries\
+    (user_id, audio_id)\
+    VALUES ($1, $2)\
+    RETURNING entry_id',
+    [userId, audioId])
+}
+
 module.exports.findByUserId = (userId) => {
   return db.manyOrNone('SELECT text, entries.entry_id, entries.created FROM entry_text INNER JOIN entries\
   ON entry_text.entry_id = entries.entry_id\
